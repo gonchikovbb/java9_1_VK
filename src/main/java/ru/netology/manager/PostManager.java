68a19b92772c63b66745761a96b38483409c1aba
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-04-05 11:54:42 +0700

    Added PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(ownerId, domain, query, ownerOnly, count, offset) {
+        return null;
+    }
+
+    public void delete(ownerId, postId) {
+    }
+}
