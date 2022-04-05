59e94f76bd40cd1f46e8809fccdf12e7a8c0cd25
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-04-05 10:44:42 +0700

    Added class

---
+++ src/main/java/ru/netology/domain/Image.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Image {
+    private String header;
+    private String image;
+    private String name;
+    private String button;
+
+    //getter+setter;
+}
---
+++ src/main/java/ru/netology/domain/Likes.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Likes {
+    private int likes;
+    private int comments;
+    private int forwards;
+    private int views;
+
+    //getter+setter;
+}
