ec4b5094b1a8b2a42331b16d9192229d2e8fb00d
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-04-08 08:43:33 +0700

    Modified PostManager, added type arguments

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -2,13 +2,15 @@ package ru.netology.manager;
 
 import ru.netology.domain.Post;
 
+import java.awt.*;
+
 public class PostManager {
     private Post[] posts;
 
-    public Post[] search(ownerId, domain, query, ownerOnly, count, offset) {
+    public Post[] search(int ownerId, String domain, String query, boolean ownerOnly, int count, int offset) {
         return null;
     }
 
-    public void delete(ownerId, postId) {
+    public void delete(int ownerId, int postId) {
     }
 }
